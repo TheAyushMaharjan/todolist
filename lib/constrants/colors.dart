d834d85c4cdfb0ee644a2e49e511051dcdff0c1e
Ayush Maharjan <Ayush.maharjan399@gmail.com>
2024-06-26 13:48:23 +0545

    commit

---
+++ lib/constrants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color tdYellow = Color(0xFFFAC375);
+const Color tdBlue = Color(0xFF84A9CD);
+const Color tdOrange = Color(0xFFFC9f66);
+
+const Color tdLYellow = Color(0xFFFAE39C);
+
+
+
