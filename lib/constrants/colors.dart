b1f88c3416c01fb5a206db12587cafd271032337
Ayush Maharjan <Ayush.maharjan399@gmail.com>
2024-07-01 14:46:52 +0545

    commit

--- lib/constrants/colors.dart
+++ lib/constrants/colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-const Color tdYellow = Color(0xFFF5F5F5);
+const Color tdYellow = Color(0xFFFFFFFF);
 const Color tdBlue = Color(0xFF84A9CD);
 const Color tdOrange = Color(0xFFFED800);
 const Color tdBlack= Color(0xFF414141);
-const Color tdLBlue = Color(0xFFE1D9D1);
+const Color tdLBlue = Color(0xFFFFFFFF);
 const Color tdRed = Color(0xFFFF0000 );
 
-const Color tdLYellow = Color(0xFFF5F5F5);
+const Color tdLYellow = Color(0xFFFFFFFF);
 
 
 
