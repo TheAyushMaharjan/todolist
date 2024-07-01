a0344326a61c3fd2cde61af09305fe58f4dbb312
Ayush Maharjan <Ayush.maharjan399@gmail.com>
2024-07-01 14:50:59 +0545

    commit

--- lib/constrants/colors.dart
+++ lib/constrants/colors.dart
@@ -4,10 +4,10 @@ const Color tdYellow = Color(0xFFFFFFFF);
 const Color tdBlue = Color(0xFF84A9CD);
 const Color tdOrange = Color(0xFFFED800);
 const Color tdBlack= Color(0xFF414141);
-const Color tdLBlue = Color(0xFFFFFFFF);
+const Color tdLBlue = Color(0xFFD8D5D5);
 const Color tdRed = Color(0xFFFF0000 );
 
-const Color tdLYellow = Color(0xFFFFFFFF);
+const Color tdLYellow = Color(0xFFF5F5F5);
 
 
 
